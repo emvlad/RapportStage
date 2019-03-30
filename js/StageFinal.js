150a7cf0c640dd984f4a978683c6ddf88d720ae0
Vladem <cevchris23@gmail.com>
2019-03-30 15:06:18 -0400

    First commit -Introduction

---
+++ js/StageFinal.js
@@ -0,0 +1,37 @@
+//préparé par l'étudiant : Chrisphonte E Vladimir
+//ID: 2015-23-208
+//Collège Montmorency
+//======== JavaScript - POO - AJAX ====================
+var payHeadline = document.querySelector("#headline");
+var payHeader = document.getElementById("header");
+//constructor function with parameters
+function Persons(fullName, jobTitle, niveauEtud) {
+  this.fullName = fullName;
+  this.id = jobTitle;
+  this.college = niveauEtud;
+  //this: stand by for all new instance
+  this.displPers =
+    "<br/>Étudiant : " +
+    this.fullName +
+    " " +
+    "<br/>ID: " +
+    this.id +
+    "<br/> " +
+    this.college;
+}
+//create new object instance of parent construtor function
+//using the operator 'new' to create a new intance of the _class_objectPersons
+var pers1 = new Persons(
+  "Chrisphonte E Vladimir",
+  "2015-23-208",
+  "Collège Montmorency"
+);
+//render student info
+payHeadline.innerHTML = pers1.displPers;
+
+//render Home_navbar
+var nav = `   <h1>Rapport Final de Stage</h1>
+  <h4>Lieu: Intelligence Industrielle</h4>
+  <h4>Du 7 janvier au 19 avril 2019</h4>
+  <a href="./">Home</a> | <a href="./component/about.html">About</a>`;
+payHeader.innerHTML = nav;
